c33d396feb40de914136de7283832a7fe1d214bd
Teresa Ogáyar De Dios <tod00001@red.ujaen.es>
2021-01-03 18:44:59 +0100

    Point 1

---
+++ src/test/java/com/example/stm/StmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
